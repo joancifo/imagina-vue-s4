7a5d3cae5dfe8d10007804856afb4239d9ae59c0
Joan Cifo <joancifo@gmail.com>
2024-10-02 10:47:37 +0200

    wip: jugant amb mòduls

--- pages/login.vue
+++ pages/login.vue
@@ -5,6 +5,9 @@ definePageMeta({
 </script>
 <template>
   <h1>Iniciar sessió</h1>
+  <img src="@/assets/descarga.jpg" alt="Logo" />
+  <img src="/descarga.jpg" />
+  <NuxtImg src="/descarga.jpg" :placeholder="[500, 500]" height="500" />
   <form>
     <input placeholder="Usuari" />
     <input placeholder="Contrasenya" type="password" />
